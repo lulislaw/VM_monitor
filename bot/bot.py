6c92b5e553eae2fe56242f2e1429c4baa731eec9
eccclips <eccclipss@gmail.com>
2023-10-15 00:10:56 +0300

    Database destroyed, keyboards added

--- bot/bot.py
+++ bot/bot.py
@@ -1,17 +1,93 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram import executor
-from keyboards import start_keyboard
+from aiogram.types import CallbackQuery
+from keyboards import start_kb, vm_add_kb, sub_lst_kb
 from settings.config import Config
-from status_base import create_base
+from status_base import createdb, page, update_lst_page, get_user_status, update_user_status, add_user
 config = Config()
 
 bot = Bot(config.token)
 dp = Dispatcher(bot)
-create_base()
+createdb()
+
+ip_addresses = [
+    "192.168.0.1",
+    "10.0.0.1",
+    "172.16.0.1",
+    "8.8.8.8",
+    "127.0.0.1",
+    "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+    "2606:2800:220:1:248:1893:25c8:1946",
+    "192.168.0.1",
+    "10.0.0.1",
+    "172.16.0.1",
+    "8.8.8.8",
+    "127.0.0.1",
+    "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+    "2606:2800:220:1:248:1893:25c8:1946",
+    "192.168.0.1",
+    "10.0.0.1",
+    "172.16.0.1",
+    "8.8.8.8",
+    "127.0.0.1",
+    "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+    "2606:2800:220:1:248:1893:25c8:1946",
+    "::1"
+]
 
 @dp.message_handler(commands=['start', 'help'])
 async def mail(message: types.Message):
-    await bot.send_message(chat_id=message.chat.id,text='helloworld!', reply_markup=start_keyboard())
+    add_user(user_id=message.chat.id, user_status="start")
+    await bot.send_message(chat_id=message.chat.id,text='helloworld!', reply_markup=start_kb())
+
+
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def mail(message: types.Message):
+    if message.text == "Добавить ВМ":
+        await bot.send_message(chat_id=message.chat.id, text='vm_add_kb!', reply_markup=vm_add_kb())
+    elif message.text == "Подписки ВМ":
+        await bot.send_message(chat_id=message.chat.id, text="ZOV", reply_markup=sub_lst_kb(ip_addresses,
+                                                                                                   message.chat.id))
+
+@dp.callback_query_handler(text="next_page")
+async def next_page_handler(callback_query: CallbackQuery):
+    await callback_query.answer()
+    user_id = callback_query.from_user.id
+
+    # Увеличиваем номер текущей страницы на 1
+    update_lst_page(user_id, int(page(user_id)) + 1)
+
+    # Обновляем сообщение с новой клавиатурой
+    await bot.edit_message_reply_markup(callback_query.from_user.id, callback_query.message.message_id,
+                                        reply_markup=sub_lst_kb(ip_addresses, user_id))
+
+# Обработчик для кнопки "Назад"
+@dp.callback_query_handler(text="prev_page")
+async def prev_page_handler(callback_query: CallbackQuery):
+    await callback_query.answer()
+    user_id = callback_query.from_user.id
+
+    # Уменьшаем номер текущей страницы на 1
+    update_lst_page(user_id, int(page(user_id)) - 1)
+
+    # Обновляем сообщение с новой клавиатурой
+    await bot.edit_message_reply_markup(callback_query.from_user.id, callback_query.message.message_id,
+                                        reply_markup=sub_lst_kb(ip_addresses, user_id))
+
+# @dp.message_handler(commands=['start', 'help'])
+# async def mail(message: types.Message):
+#     await bot.send_message(chat_id=message.chat.id, text='helloworld!', reply_markup=start_keyboard())
+#
+#
+# @dp.message_handler(commands=['start', 'help'])
+# async def mail(message: types.Message):
+#     await bot.send_message(chat_id=message.chat.id, text='helloworld!', reply_markup=start_keyboard())
+#
+#
+# @dp.message_handler(commands=['start', 'help'])
+# async def mail(message: types.Message):
+#     await bot.send_message(chat_id=message.chat.id, text='helloworld!', reply_markup=start_keyboard())
+#
 
 
 if __name__ == '__main__':
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,10 +1,80 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, KeyboardButton, ReplyKeyboardMarkup
-
+from status_base import page, update_lst_page
 # списоук вм списоук подписок и добавить ещЙо
 
-def start_keyboard():
+
+def start_kb():
     pool_vm_button = KeyboardButton(text='Список ВМ')
-    subs_vm_button = KeyboardButton(text='Подпискм ВМ')
+    subs_vm_button = KeyboardButton(text='Подписки ВМ')
     add_vm_button = KeyboardButton(text='Добавить ВМ')
 
     return ReplyKeyboardMarkup(row_width=3).add(pool_vm_button,subs_vm_button,add_vm_button)
+
+def vm_add_kb():
+    ip_bt = InlineKeyboardButton(text='ip_bt', callback_data="1")
+    port_bt = InlineKeyboardButton(text='port_bt', callback_data="2")
+    username_bt = InlineKeyboardButton(text='username_bt', callback_data="3")
+    password_bt = InlineKeyboardButton(text='password_bt', callback_data="4")
+    accept_vm_add_bt = InlineKeyboardButton(text='accept_vm_add_bt', callback_data="5")
+
+    return InlineKeyboardMarkup(row_width=2).add(ip_bt, port_bt, username_bt, password_bt, accept_vm_add_bt)
+
+def create_reply_keyboard(lst_titles, symbol=''):
+    num_buttons = len(lst_titles)
+    MAX_BUTTONS_PER_ROW = 4
+    num_cols = min(num_buttons, MAX_BUTTONS_PER_ROW)
+    num_rows = (num_buttons + num_cols - 1) // num_cols
+    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+    for i in range(num_rows):
+        row_buttons = []
+        for j in range(num_cols):
+            button_index = i * num_cols + j
+            if button_index >= num_buttons:
+                break
+            button_text = f"{symbol}{lst_titles[button_index]}"
+            button_callback = f"account_button"
+            row_buttons.append(InlineKeyboardButton(button_text, callback_data=button_callback))
+        keyboard.row(*row_buttons)
+
+    return keyboard
+
+# def sub_lst_kb(lst: list, user_id):
+#     vm_list_length = len(lst)
+#     if page(user_id) == None:
+#         update_lst_page(user_id, 1)
+#     if vm_list_length < 10:
+#         return create_reply_keyboard(lst)
+def sub_lst_kb(lst: list, user_id):
+    vm_list_length = len(lst)
+    current_page = page(user_id) if page(user_id) != 0 else 1
+
+    # Количество элементов на одной странице
+    items_per_page = 7
+
+    # Вычисляем начало и конец текущей страницы
+    start_idx = (current_page - 1) * items_per_page
+    end_idx = min(start_idx + items_per_page, vm_list_length)
+
+    # Создаем клавиатуру для текущей страницы
+    keyboard = create_reply_keyboard(lst[start_idx:end_idx])
+
+    # Добавляем кнопки "Назад" и "Дальше" при необходимости
+    if current_page > 1:
+        keyboard.row(InlineKeyboardButton('Назад', callback_data='prev_page'))
+    if end_idx < vm_list_length:
+        keyboard.row(InlineKeyboardButton('Дальше', callback_data='next_page'))
+
+    return keyboard
+def start_keyboard():
+    pool_vm_button = InlineKeyboardButton(text='Список ВМ')
+    subs_vm_button = InlineKeyboardButton(text='Подпискм ВМ')
+    add_vm_button = InlineKeyboardButton(text='Добавить ВМ')
+
+    return InlineKeyboardMarkup(row_width=3).add(pool_vm_button,subs_vm_button,add_vm_button)
+
+def start_keyboard():
+    pool_vm_button = InlineKeyboardButton(text='Список ВМ')
+    subs_vm_button = InlineKeyboardButton(text='Подпискм ВМ')
+    add_vm_button = InlineKeyboardButton(text='Добавить ВМ')
+
+    return InlineKeyboardMarkup(row_width=3).add(pool_vm_button,subs_vm_button,add_vm_button)
